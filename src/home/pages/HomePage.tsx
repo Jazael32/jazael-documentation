b97add0fb8ffd67207b90726453a0c75e5db1cf7
Martin J Moreno <42747172+Jazael32@users.noreply.github.com>
2026-05-11 18:53:36 -0500

    Small change

--- src/home/pages/HomePage.tsx
+++ src/home/pages/HomePage.tsx
@@ -1,4 +1,3 @@
-import { Header } from "@/components/shared/Header"
 import { Footer } from "@/components/shared/Footer"
 import { HeroPage } from "./HeroPage"
 import { AboutPage } from "./AboutPage"
